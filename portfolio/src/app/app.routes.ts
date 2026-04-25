985fa4419af6f0fd005ab29254a736bb4e158955
Nicholas Mountain <nmountain@unc.edu>
2026-04-24 20:59:54 -0400

    add nav bar with routes to basic info pages

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Projects } from './pages/projects/projects';
+import { WorkExperience } from './pages/work-experience/work-experience';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: 'work-experience', component: WorkExperience },
   { path: '**', redirectTo: '' },
 ];
---
+++ portfolio/src/app/pages/work-experience/work-experience.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience',
+  imports: [],
+  templateUrl: './work-experience.html',
+  styleUrl: './work-experience.css',
+})
+export class WorkExperience {
+
+}
