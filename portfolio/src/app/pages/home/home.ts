cf479c40ac7257a3cd45a86fcaf9ce91505b85e7
Nicholas Mountain <nmountain@unc.edu>
2026-04-24 21:24:13 -0400

    set up homepage scrolling

--- portfolio/src/app/pages/home/home.ts
+++ portfolio/src/app/pages/home/home.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, MatChipsModule, MatDividerModule, MatIconModule],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-  skills = ['Angular', 'TypeScript', 'Node.js', 'Python', 'SQL', 'Git'];
-}
+export class Home {}
