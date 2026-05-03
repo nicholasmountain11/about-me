df5ce1f6f9774d8f0f167997c969cfce520e4055
Nicholas Mountain <nmountain@unc.edu>
2026-05-03 17:44:15 -0400

    remove matToolTip dependencies

--- portfolio/src/app/pages/home/home.ts
+++ portfolio/src/app/pages/home/home.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-home',
-  imports: [MatTooltipModule],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
